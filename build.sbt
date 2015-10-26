5fabf04afefafb36415ae2d41957f1ad9baaab71
Peng.Li <peng.li@rea-group.com>
2015-10-26 21:53:44 +0800

    can hold doc events now

--- build.sbt
+++ build.sbt
@@ -11,7 +11,7 @@ ivyScala := ivyScala.value map {
 }
 
 libraryDependencies in ThisBuild ++= Seq(
-  "com.thoughtworks" %% "remote-pair-server" % "0.2.6",
+  "com.thoughtworks" %% "remote-pair-server" % "0.4.0",
   "commons-lang" % "commons-lang" % "2.6",
   "commons-io" % "commons-io" % "2.0.1",
   "org.scalaz" %% "scalaz-core" % "7.1.3",
@@ -19,10 +19,18 @@ libraryDependencies in ThisBuild ++= Seq(
   "org.json4s" %% "json4s-native" % "3.2.11",
   "org.json4s" %% "json4s-core" % "3.2.11",
   "org.json4s" %% "json4s-ext" % "3.2.11",
+  "com.github.julien-truffaut" %% "monocle-core" % "1.2.0-M1",
+  "com.github.julien-truffaut" %% "monocle-generic" % "1.2.0-M1",
+  "com.github.julien-truffaut" %% "monocle-macro" % "1.2.0-M1",
+  "com.github.julien-truffaut" %% "monocle-state" % "1.2.0-M1",
   "ch.qos.logback" % "logback-classic" % "1.1.2",
   "org.slf4j" % "slf4j-api" % "1.7.7",
   "org.specs2" %% "specs2-mock" % "2.4.2" % "test",
   "org.specs2" %% "specs2" % "2.4.2" % "test",
   "org.apache.commons" % "commons-vfs2" % "2.0" % "test",
+  "com.github.julien-truffaut" %% "monocle-law" % "1.2.0-M1" % "test",
   "io.netty" % "netty-all" % "5.0.0.Alpha1"
 )
+
+// for @Lenses macro support
+addCompilerPlugin("org.scalamacros" %% "paradise" % "2.0.1" cross CrossVersion.full)
--- src/main/scala/com/thoughtworks/pli/remotepair/monitor/MainDialog.scala
+++ src/main/scala/com/thoughtworks/pli/remotepair/monitor/MainDialog.scala
@@ -1,7 +1,14 @@
 package com.thoughtworks.pli.remotepair.monitor
 
-import SwingVirtualImplicits._
-import io.netty.channel.{ChannelHandlerContext, ChannelHandlerAdapter}
+import javax.swing.tree.{DefaultMutableTreeNode, DefaultTreeModel}
+
+import com.thoughtworks.pli.intellij.remotepair.protocol._
+import com.thoughtworks.pli.intellij.remotepair.utils.ContentDiff
+import com.thoughtworks.pli.remotepair.monitor.SwingVirtualImplicits._
+import io.netty.channel.{ChannelHandlerAdapter, ChannelHandlerContext}
+import monocle.function.Each._
+import monocle.macros.Lenses
+import monocle.std.list._
 
 import scala.util.Try
 
@@ -10,19 +17,69 @@ object MainDialog extends _MainDialog {
   private var nettyClient: Option[NettyClient] = None
 
   case class ServerAddress(ip: String, port: Int)
+
+  private val parseEvent = new ParseEvent
+
   private def connectToServer(serverAddress: ServerAddress): Unit = {
-    if (nettyClient.isEmpty) nettyClient = Some(new NettyClient(serverAddress))
+    if (nettyClient.isEmpty) nettyClient = Some(new NettyClient(serverAddress)(parseEvent))
     nettyClient.foreach { client =>
       client.connect(new ChannelHandlerAdapter {
-
         override def channelActive(ctx: ChannelHandlerContext): Unit = {
-          ctx.writeAndFlush(???)
+          ctx.writeAndFlush(ImMonitor.toMessage)
         }
         override def channelRead(ctx: ChannelHandlerContext, msg: scala.Any): Unit = {
-          ???
+          msg match {
+            case event: ServerStatusResponse => handleServerStatusResponse(event)
+            case MonitorEvent(projectName, realEventMessage, _) => parseEvent(realEventMessage) match {
+              case event: CreateDocumentConfirmation => handleCreateDocumentConfirmation(projectName, event)
+              case event: ChangeContentConfirmation => handleChangeContentConfirmation(projectName, event)
+              case other => println("### s" + other.toMessage)
+            }
+            case _ => ???
+          }
         }
       })
     }
+  }
+
+  @Lenses("_") case class Change(version: Int, diffs: List[ContentDiff])
+  @Lenses("_") case class Doc(path: String, baseVersion: Int, baseContent: Content, changes: List[Change] = Nil)
+  @Lenses("_") case class Project(name: String, docs: List[Doc] = Nil)
+  @Lenses("_") case class Projects(projects: List[Project] = Nil)
+
+  import Project._
+  import Projects._
+
+
+  private var projects: Projects = Projects()
+
+  private def handleChangeContentConfirmation(projectName: String, event: ChangeContentConfirmation): Unit = {
+    val change = Change(event.newVersion, event.diffs.toList)
+    def addChange(doc: Doc): Doc = if (doc.path == event.path) doc.copy(changes = change :: doc.changes) else doc
+    projects = (_projects composeTraversal each composeLens _docs composeTraversal each).modify(addChange)(projects)
+  }
+
+  private def handleCreateDocumentConfirmation(projectName: String, event: CreateDocumentConfirmation) = {
+    val doc = Doc(event.path, baseVersion = event.version, baseContent = event.content)
+    def addDoc(docs: List[Doc]): List[Doc] = (doc :: docs).sortBy(_.path)
+    projects = (_projects composeTraversal each composeLens _docs).modify(addDoc)(projects)
+    createTree()
+  }
+
+  private def handleServerStatusResponse(event: ServerStatusResponse) = {
+    projects = Projects(event.projects.map(info => Project(info.name)).toList)
+    createTree()
+  }
+
+  private def createTree(): Unit = {
+    val root = new DefaultMutableTreeNode("projects")
+    projects.projects.foreach(p => {
+      val pNode = new DefaultMutableTreeNode(p.name)
+      p.docs.foreach(d => pNode.add(new DefaultMutableTreeNode(d.path)))
+      root.add(pNode)
+    })
+    fileTree.setModel(new DefaultTreeModel(root))
+    fileTree.updateUI()
   }
 
   private def getInputServerAddress(): Option[ServerAddress] = {
@@ -33,16 +90,16 @@ object MainDialog extends _MainDialog {
   }
 
   connectButton.onClick {
+    println("### clicked on connectButton")
     getInputServerAddress() match {
-      case Some(ServerAddress(ip, port)) => connectToServer(ip, port)
-      case _ =>
+      case Some(serverAddress) => connectToServer(serverAddress)
+      case _ => serverAddressTextField.requestFocus()
     }
   }
 
   def main(args: Array[String]) {
-    val dialog = new _MainDialog
-    dialog.pack()
-    dialog.setVisible(true)
+    this.pack()
+    this.setVisible(true)
     System.exit(0)
   }
 
